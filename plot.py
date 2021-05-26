fd1c1f685f6518ae42052a99a3ead140391ee945
Chuyu Yang <59849594+bluesycy@users.noreply.github.com>
2021-05-26 14:31:39 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -15,3 +15,19 @@ plt.eventplot(interval_spike_times[neuron_idx], color=".2")
 
 _ = widgets.interact(Myplot_E_diffI_difftau, I_ext=(0.0, 10., 1.),
                      tau=(1., 5., 0.2))
+
+
+##seaborn
+import pandas as pd
+d = {'Normalized firing rate': firing_list, 'pext': pext_list,
+     'p0':self_excitation_list}
+df = pd.DataFrame(data=d)
+import seaborn as sns
+import matplotlib.pyplot as plt
+plt.figure(dpi=250)
+# Draw a nested boxplot to show bills by day and time
+sns.boxplot(x="pext", y="Normalized firing rate",
+            hue="p0", palette=["m", "g","r"],
+            data=df)
+plt.axhline(1,linestyle='dashed',color='gray')
+# sns.despine(offset=10, trim=True)
